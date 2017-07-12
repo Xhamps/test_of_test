fab6651180254df9f99d3ae5cbf1634f2b8f5a65
Thiago Paixão <thiago.paixao@vivareal.com>
2017-07-12 18:45:44 -0300

    jest workflow restart

---
+++ jest/tests/basic.test.js
@@ -0,0 +1,27 @@
+import math from '../../app/math';
+let sumMock;
+
+beforeEach(() => {
+  sumMock = jest.spyOn(math, 'sum');
+});
+
+afterEach(() => {
+  sumMock.mockRestore();
+  sumMock.mockReset();
+});
+
+test('sum(3) should equals 6', () => {
+  expect(math.sum(3)).toEqual(6);
+});
+
+test('method with private method call', () =>{
+  math.multiply(2);
+
+  expect(math.sum).toHaveBeenCalledTimes(2);
+});
+
+test('using the same spy in another test', () => {
+  math.multiply(1);
+
+  expect(math.sum).toHaveBeenCalledTimes(1);
+});
