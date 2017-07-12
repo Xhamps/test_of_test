f77883e146e98a01962d40842f1faaacd16ca924
Thiago Paixão <thiago.paixao@vivareal.com>
2017-07-12 18:49:08 -0300

    refactoring basic

--- jest/tests/basic.test.js
+++ jest/tests/basic.test.js
@@ -1,27 +1,29 @@
 import math from '../../app/math';
-let sumMock;
 
-beforeEach(() => {
-  sumMock = jest.spyOn(math, 'sum');
-});
+describe('Math', () => {
+  let sumMock;
 
-afterEach(() => {
-  sumMock.mockRestore();
-  sumMock.mockReset();
-});
+  beforeEach(() => {
+    sumMock = jest.spyOn(math, 'sum');
+  });
 
-test('sum(3) should equals 6', () => {
-  expect(math.sum(3)).toEqual(6);
-});
+  afterEach(() => {
+    sumMock.mockRestore();
+  });
 
-test('method with private method call', () =>{
-  math.multiply(2);
+  test('sum(3) should equals 6', () => {
+    expect(math.sum(3)).toEqual(6);
+  });
 
-  expect(math.sum).toHaveBeenCalledTimes(2);
-});
+  test('method with private method call', () =>{
+    math.multiply(2);
 
-test('using the same spy in another test', () => {
-  math.multiply(1);
+    expect(math.sum).toHaveBeenCalledTimes(2);
+  });
 
-  expect(math.sum).toHaveBeenCalledTimes(1);
-});
+  test('using the same spy in another test', () => {
+    math.multiply(1);
+
+    expect(math.sum).toHaveBeenCalledTimes(1);
+  });
+})
